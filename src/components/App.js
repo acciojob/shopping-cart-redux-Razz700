c3ce968b0193414db0b1b9f3e9526fa9a01be4da
Razzakh <mrazzakh123@gmail.com>
2024-07-23 15:07:01 +0530

    th3

--- src/components/App.js
+++ src/components/App.js
@@ -86,7 +86,7 @@ count[i]!=1? setcount(count=>{
 const [coupon,setcoupon]=useState(false);
   return (
     <div className="navbar-expand-lg">
-      <div><h3></h3></div>
+      <div><h3> .</h3></div>
       <h2 className="text-center">Shopping Cart</h2>
       <div className="all custom-card card" >
          <h3>All Products</h3>
