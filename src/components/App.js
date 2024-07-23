6737dc5ce0c1d74a79b7d6d735074a59fd4de06a
Razzakh <mrazzakh123@gmail.com>
2024-07-23 13:41:50 +0530

    next3

--- src/components/App.js
+++ src/components/App.js
@@ -84,10 +84,8 @@ count[i]!=1? setcount(count=>{
 }
 const [coupon,setcoupon]=useState(false);
   return (
-    <div >
-      <div className="navbar-expand-lg">
+    <div className="navbar-expand-lg">
         <h2 className="text-center">Shopping Cart</h2>
-      </div>
       <div id="all-products">
       <h3>All Products</h3>
       <div className="all custom-card card" >
@@ -95,7 +93,7 @@ const [coupon,setcoupon]=useState(false);
        return <div className="item card" key={'a'+i}>
           <img src={item.images[0]} />
           <p>${item.price}</p>
-          <p>{item.title}</p>
+          <h4>{item.title}</h4>
           <button className="btn-primary" onClick={()=>handleaddtocart(item)}>Add to Cart</button>
        </div>
       })}
@@ -108,7 +106,7 @@ const [coupon,setcoupon]=useState(false);
        return <div className="item custom-card card card-body" key={'w'+i}>
           <img src={item.images[0]} />
           <p>${item.price}</p>
-          <p>{item.title}</p>
+          <h4>{item.title}</h4>
           <button className="btn-primary btn" onClick={()=>handleaddtocart(item,'wishlist')}>Add to Cart</button>
        </div>
       })}
@@ -127,9 +125,9 @@ const [coupon,setcoupon]=useState(false);
           <div className="p-lastbtn card-body">
           <div className="details"><p>Price:<br/>${item.price}</p>
           <p>Product Name:<br/>{item.title}</p></div>
-          <div className="last-btn card-body"><button className="btn" onClick={()=>handleincrement('dec',i,item)}>-</button><span>{count[i]}</span><button className="btn" onClick={()=>handleincrement('inc',i,item)}>+</button></div>
+          <div className="last-btn"><button className="btn" onClick={()=>handleincrement('dec',i,item)}>-</button><span>{count[i]}</span><button className="btn" onClick={()=>handleincrement('inc',i,item)}>+</button></div>
           </div>
-        <div className="btn1">
+        <div className="btn1 card-body">
           <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
 </svg>
