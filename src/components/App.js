0b5edfe18cbe9d9080ff688a1c4f208853ba4eeb
Razzakh <mrazzakh123@gmail.com>
2024-07-24 06:57:05 +0530

    check23

--- src/components/App.js
+++ src/components/App.js
@@ -15,10 +15,13 @@ const App = () => {
  console.log(wishlist,'list');
  console.log('total',total);
     useEffect(()=>{
+    const completetdata=[{"id":632,"title":"Trustworthiness","price":186,"description":"Vitae labore sunt eo","images":["https://static.india.com/wp-content/uploads/2020/05/table-tennis.jpg?impolicy=Medium_Widthonly&w=700"],"creationAt":"2024-07-23T07:13:05.000Z","updatedAt":"2024-07-23T07:13:05.000Z","category":{"id":1,"name":"Clothes","image":"https://i.imgur.com/QkIa5tT.jpeg","creationAt":"2024-07-23T04:38:02.000Z","updatedAt":"2024-07-23T04:38:02.000Z"}},{"id":638,"title":"Quest","price":186,"description":"Vitae labore sunt eo","images":["https://static.india.com/wp-content/uploads/2020/05/table-tennis.jpg?impolicy=Medium_Widthonly&w=700"],"creationAt":"2024-07-23T07:13:06.000Z","updatedAt":"2024-07-23T07:13:06.000Z","category":{"id":4,"name":"Shoes","image":"https://api.escuelajs.co/api/v1/files/4184.jpg","creationAt":"2024-07-23T04:38:02.000Z","updatedAt":"2024-07-23T10:37:20.000Z"}},{"id":640,"title":"Uplift","price":186,"description":"Vitae labore sunt eo","images":["https://static.india.com/wp-content/uploads/2020/05/table-tennis.jpg?impolicy=Medium_Widthonly&w=700"],"creationAt":"2024-07-23T07:13:06.000Z","updatedAt":"2024-07-23T07:13:06.000Z","category":{"id":3,"name":"Furniture","image":"https://i.imgur.com/Qphac99.jpeg","creationAt":"2024-07-23T04:38:02.000Z","updatedAt":"2024-07-23T04:38:02.000Z"}},{"id":641,"title":"Ambit","price":186,"description":"Vitae labore sunt eo","images":["https://static.india.com/wp-content/uploads/2020/05/table-tennis.jpg?impolicy=Medium_Widthonly&w=700"],"creationAt":"2024-07-23T07:13:06.000Z","updatedAt":"2024-07-23T22:31:57.000Z","category":{"id":5,"name":"Miscellaneous","image":"https://i.imgur.com/BG8J0Fj.jpg","creationAt":"2024-07-23T04:38:02.000Z","updatedAt":"2024-07-23T04:38:02.000Z"}},{"id":643,"title":"Quest","price":186,"description":"Vitae labore sunt eo","images":["https://static.india.com/wp-content/uploads/2020/05/table-tennis.jpg?impolicy=Medium_Widthonly&w=700"],"creationAt":"2024-07-23T07:13:06.000Z","updatedAt":"2024-07-23T07:13:06.000Z","category":{"id":3,"name":"Furniture","image":"https://i.imgur.com/Qphac99.jpeg","creationAt":"2024-07-23T04:38:02.000Z","updatedAt":"2024-07-23T04:38:02.000Z"}}];
+    dispatch(setalldata(completetdata));
       axios.request('https://api.escuelajs.co/api/v1/products').then((data)=>{
         console.log(data);
         const arrthis=data.data.slice(0,5);
-        dispatch(setalldata(arrthis));
+        console.log(arrthis,JSON.stringify(arrthis));
+        
         }).catch((e)=>{console.log(e);}); 
   },[]);
 const handleaddtocart=(item,check)=>{
@@ -140,7 +143,7 @@ const [coupon,setcoupon]=useState(false);
           <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
 </svg>
-<button className="btn" onClick={()=>handlecartremove(item)}>Remove item</button></div>
+<button className="btn btn-primary" onClick={()=>handlecartremove(item)}>Remove item</button></div>
 <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
 </svg><button className="btn" onClick={()=>handlewishlist(item)}>Move to Wishlist</button></div>
